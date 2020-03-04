a6557bb9fa2ff121507b42f3eaae65bd298af30a
Sönke <soenke@element.in>
2020-03-04 16:06:59 +0100

    Add about page

---
+++ src/About.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Home.css';
+
+export default function About() {
+    return (
+      <div>
+        <p> You've been routed to the About Page </p>
+      </div>
+    );
+}
--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import Route from 'react-router-dom/Route';
 import Switch from 'react-router-dom/Switch';
 import Home from './Home';
 import './App.css';
+import About from './About';
 
 const App = () => (
   <Switch>
     <Route exact path="/" component={Home} />
+    <Route exact path="/about" component={About} />
   </Switch>
 );
 
